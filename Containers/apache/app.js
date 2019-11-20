9b95332f24c48dd7acfb16f32ce7f48f8716aff7
John S. Tonello <21206201+jtonello@users.noreply.github.com>
2019-11-20 17:34:25 -0500

    Update app.js

--- Containers/apache/app.js
+++ Containers/apache/app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const https = require('https');
 const os = require('os');
 
 console.log("Kubia server starting...");
@@ -11,4 +12,5 @@ var handler = function(request, response) {
  
  var www = http.createServer(handler);
  www.listen(8080);
- www.listen(8443);
+ var mywww = http.createServer(handler);
+ mywww.listen(8443);
